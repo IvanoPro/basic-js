ca5138ec1e43b2fccaace4bbcf2a98ed41c286c3
Ivan Gawr <ivangawr1993>
2020-09-29 23:23:36 +0300

    fix: implement Carbon dating task is true

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,8 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  let num = parseFloat(sampleActivity);
+  const K = 0.693 / HALF_LIFE_PERIOD;
+  return (Number.isNaN(num) || (typeof sampleActivity !== 'string') || (num <= 0 || num > 15))?false: Math.ceil((Math.log(MODERN_ACTIVITY / num)) / K);
 };
