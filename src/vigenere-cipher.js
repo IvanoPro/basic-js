560ad0f70859135c7559d15fb3c756a786ec1111
Ivan Gawr <ivangawr1993>
2020-10-04 22:42:30 +0300

    feat: add other tasks

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(dir = true) {
+    this.dir = dir;
+  }
+
+  encrypt(str, key) {
+    return this.crypt(str, key);
+  }
+
+  decrypt(str, key) {
+    return this.crypt(str, key, 'decrypt');
+  }
+
+  crypt(str, key, mode){
+    if (!str && !key) throw Error()
+    str = str.toUpperCase();
+    key = key.toUpperCase();
+    let arrRes = [];
+    for (let i = 0, j = 0; i < str.length; i++) {
+      if(str[i].match(/[A-Z]/)){
+        if(mode == 'decrypt') {
+          arrRes.push(String.fromCharCode((str[i].charCodeAt(0) - 'A'.charCodeAt(0) + (26 - (key[j % key.length].charCodeAt(0) - 'A'.charCodeAt(0)))) % 26 + 'A'.charCodeAt(0)));
+        } else {
+          arrRes.push(String.fromCharCode((str[i].charCodeAt(0) - 'A'.charCodeAt(0) + (key[j % key.length].charCodeAt(0) - 'A'.charCodeAt(0))) % 26 + 'A'.charCodeAt(0)));
+        }
+          j++
+        } else {
+          arrRes.push(str[i])
+        } 
+      }
+    return  this.dir ? arrRes.join('') : arrRes.reverse().join('')
   }
 }
-
 module.exports = VigenereCipheringMachine;
